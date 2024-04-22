0a1dcd3c484d6df6705ca0f2fe5adea34bf9b54f
eramoss <edulramos@outlook.com>
2024-04-22 09:49:06 -0300

    fix: changed api from /full to normal

--- domain/src/main/java/com/mangabit/domain/manga/Fetcher.kt
+++ domain/src/main/java/com/mangabit/domain/manga/Fetcher.kt
@@ -4,11 +4,11 @@ import okhttp3.Response
 
 class Fetcher {
     companion object {
-        const val api = "https://api.jikan.moe/v4/manga/"
+        private const val API = "https://api.jikan.moe/v4/manga/"
         fun fetchManga(id: Int): Response {
             return okhttp3.OkHttpClient().newCall(
                 okhttp3.Request.Builder()
-                    .url("$api$id/full")
+                    .url(API + id)
                     .build()
             ).execute()
         }
